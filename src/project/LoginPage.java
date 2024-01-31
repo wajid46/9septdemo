0a5e38edc804a42dddae4f8c170e46dc6dbb47ee
username <157899060+wajid46@users.noreply.github.com>
2024-01-31 17:10:11 +0530

    Added basic infrastructure of framework

---
+++ src/project/LoginPage.java
@@ -0,0 +1,18 @@
+package project;
+
+public class LoginPage {
+
+	
+		public void logintoApp()
+		
+		
+		{
+			System.out.println("Login to App");
+		}
+		
+		public void fetchTitle()
+		{
+			System.out.println("Getting title of page");
+		}
+	}
+
